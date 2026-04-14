e538a7c917ccde439868666de1e250e9cd6ebbd9
Astha Kharadi <asthakharadi2511@gmail.com>
2026-04-14 12:38:52 +0530

    xcc

--- src/pages/Homepage/About/About.jsx
+++ src/pages/Homepage/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import "./About.scss";
-import aboutImg from "../../../assets/About/About1.png";
+import aboutImg from "../../../assets/About/about1.png";
 import trailArrowIcon from "../../../assets/Hero/arrow.png";
 import { MdKeyboardDoubleArrowUp } from "react-icons/md";
 
